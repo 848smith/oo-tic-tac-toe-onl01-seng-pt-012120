4bcc30f9a4c5f165df20bf033160bdb05c0ae60c
Andrew Smith <848smith@gmail.com>
2020-02-17 05:36:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,16 @@ class TicTacToe
   end
   
   def winner
-    
+    WIN_COMBINATIONS.detect do |combo|
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+        "X"
+      elsif
+        @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        "O"
+      elsif
+        nil
+      end
+    end
   end
 end
 
