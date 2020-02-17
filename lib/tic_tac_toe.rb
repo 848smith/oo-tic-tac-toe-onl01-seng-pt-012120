d8e4a11219372007ad265ef56d7f8065008f56ef
Andrew Smith <848smith@gmail.com>
2020-02-17 04:59:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,7 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     token = current_player
+    
     if valid_move?(index)
       move(index, token)
       display_board
@@ -41,6 +42,8 @@ class TicTacToe
       turn
     end
   end
+  
+  
 end
 
 
